7183be9b8b0abecbf9e829739bca1e24c084f14e
Chirus Mina <chirusmina@gmail.com>
2024-03-05 21:39:23 +0200

    rezolvare exercitii

---
+++ src/me/mina/main/Exercitiu1.java
@@ -0,0 +1,11 @@
+package me.mina.main;
+
+public class Exercitiu1 {
+
+    public static void rezolva(){
+        for (int i = 3; i <= 99; i+=3) {
+            System.out.print(i+" ");
+        }
+    }
+
+}
