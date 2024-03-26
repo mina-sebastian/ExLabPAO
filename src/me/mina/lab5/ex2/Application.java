ab8de216dbdf5bd6e6602ed719b6eb743ac399ed
Chirus Mina <chirusmina@gmail.com>
2024-03-26 14:48:17 +0200

    rezolvare exercitiu stringuri laborator 5

---
+++ src/me/mina/lab5/ex2/Application.java
@@ -0,0 +1,19 @@
+package me.mina.lab5.ex2;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class Application {
+    public static void main(String[] args) {
+        String str = "Odata creat un sir de caractere cu continutul sau nu mai poate fi modificat.";
+
+        System.out.println("Lungimea sirului: "+str.length());
+        System.out.println("Doar litere mari si litere mici: "+str.matches("[a-zA-Z]*"));
+
+        String[] splits = str.split(" ");
+        System.out.println("Cuvintele din mijloc: "+splits[splits.length/2-1]+" "+splits[splits.length/2]);
+
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println("Sirul inversat: "+sb.reverse());
+    }
+}
