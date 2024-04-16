ba2cad4451af5f4956c9e2055aaab2a80f6b550e
Chirus Mina <chirusmina@gmail.com>
2024-04-16 19:27:48 +0300

    rezolvare laborator 8

--- src/me/mina/lab8/ex2/MainEx2CitireOcteti.java
+++ src/me/mina/lab8/ex2/MainEx2CitireOcteti.java
@@ -4,25 +4,33 @@ import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainEx2CitireOcteti {
 
     public static void main(String[] args) {
-
+        List<Persoana> persoane = new ArrayList<>();
         try (ObjectInputStream reader = new ObjectInputStream(new FileInputStream("persoaneOcteti.txt"))) {
             Persoana persoana;
             while (true) {
                 try {
                     persoana = (Persoana) reader.readObject();
-                    if (persoana.getNume().equals(persoana.getPrenume())) {
-                        System.out.println(persoana.getNume() + " " + persoana.getPrenume() + " " + persoana.getVarsta() + " " + persoana.getSuma() + " " + persoana.getValuta());
-                    }
+                    persoane.add(persoana);
                 } catch (EOFException e) {
                     break;
                 }
             }
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
+        }
+
+        for (int i = 0; i < persoane.size(); i++) {
+            for (int j = 0; j < persoane.size(); j++) {
+                if(i != j && persoane.get(i).getNume().equals(persoane.get(j).getNume()) && persoane.get(i).getPrenume().equals(persoane.get(j).getPrenume())) {
+                    System.out.println(persoane.get(i));
+                }
+            }
         }
 
     }
